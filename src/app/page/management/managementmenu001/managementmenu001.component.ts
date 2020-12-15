0f24f64bd0f52c0c083dbee308a66b8f2d79676d
unknown <nattaphat.mike@gmail.com>
2020-12-15 10:40:57 +0700

    test

--- src/app/page/management/managementmenu001/managementmenu001.component.ts
+++ src/app/page/management/managementmenu001/managementmenu001.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./managementmenu001.component.css']
 })
 export class Managementmenu001Component implements OnInit {
-
+  public show:boolean = false;
+  public buttonName:any = 'Show';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  show_search_document_number_day(){
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
   }
 
 }
