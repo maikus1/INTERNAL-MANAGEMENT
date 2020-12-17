01ac62aeb3dc044772fa57645f54b924fa8f5647
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2020-12-17 11:15:47 +0700

    add

--- src/app/page/user/user.module.ts
+++ src/app/page/user/user.module.ts
@@ -4,16 +4,18 @@ import { HomeuserComponent } from './homeuser/homeuser.component';
 import { Usermenu001Component } from './usermenu001/usermenu001.component';
 import { Usermenu002Component } from './usermenu002/usermenu002.component';
 import { Routes, RouterModule } from '@angular/router';
-import { ShareModule } from '../../share/share.module'
+import { ShareModule } from '../../share/share.module';
+import { Usermenu002DetailComponent } from './usermenu002-detail/usermenu002-detail.component'
 
 const routes: Routes = [
   { path: 'homeuser', component: HomeuserComponent },
   { path: 'usermenu001', component: Usermenu001Component },
   { path: 'usermenu002', component: Usermenu002Component },
+  { path: 'Usermenu002DetailComponent', component: Usermenu002DetailComponent },
 ]
 
 @NgModule({
-  declarations: [HomeuserComponent, Usermenu001Component, Usermenu002Component],
+  declarations: [HomeuserComponent, Usermenu001Component, Usermenu002Component, Usermenu002DetailComponent],
 
   imports: [
     CommonModule,
